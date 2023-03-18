650d2146ec821241dce3d69ba474bb7f31b99009
Daniel Kusi Boateng <daniel.boateng@amalitech.org>
2023-03-18 10:15:27 +0000

    new update

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -2,7 +2,23 @@ import React from "react";
 
 const Header = () => {
     
-
+    return (
+        <div>
+<div className={`header ${darkMode ? 'darkMode': ""}`}>
+    <div className="header-container">
+    <h2 className="logo">Where in the world?</h2>
+    
+    <div className="switch-mode" onClick={onClick}>
+    < i className='fas fa-moon'></i>
+    <h3>Dark Mode</h3>
+    </div>
+   
+    </div>
+    
+</div>
+</div>
+    )
+    
 }
 
 export default Header;
