9d22ce17438274b41052ff0680fb5fce5baf4570
Daniel Kusi Boateng <daniel.boateng@amalitech.org>
2023-03-18 20:53:34 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,16 @@ import CountryDetails from './Country/CountryDetails';
 interface CountryData {
   alpha3Code: string;
   name: string;
+  nativeName: string;
   capital: string;
   population: number;
   region: string;
+  subregion: string;
   flag: string;
+  topLevelDomain: string;
+  currencies: { name: string }[];
+  languages: { name: string }[];
+  borders: string[];
 }
 
 function App() {
